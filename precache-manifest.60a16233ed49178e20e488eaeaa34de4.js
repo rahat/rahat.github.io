851bc9aa7fc470b711c0caf04ab0e7044f64c691
Rahat Mahmud <rahat.mahmud@macaulay.cuny.edu>
2020-06-13 18:16:47 -0400

    Initial commit

---
+++ precache-manifest.60a16233ed49178e20e488eaeaa34de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1961eb3900708723dd39ee25e38af253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef02fdf4c4c3c8e4297",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d11ba0341c87ad5902a8",
+    "url": "/static/js/2.4ac37575.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4ac37575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef02fdf4c4c3c8e4297",
+    "url": "/static/js/main.71f7d226.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  }
+]);
